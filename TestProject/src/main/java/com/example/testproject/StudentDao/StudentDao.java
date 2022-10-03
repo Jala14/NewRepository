2ff5167bc2a14c918391d9cfcde0715085d4b953
BNihad <babazadenihad331@gmail.com>
2022-10-03 15:09:34 +0400

    NewTest

---
+++ TestProject/src/main/java/com/example/testproject/StudentDao/StudentDao.java
@@ -0,0 +1,13 @@
+package com.example.testproject.StudentDao;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Data
+public class StudentDao {
+    Integer a;
+    Integer b;
+
+}
